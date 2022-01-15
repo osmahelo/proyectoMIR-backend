b8efe91c918448d15c2811c5c4dc2a56380dcf4a
Laura <laurafcanon@gmail.com>
2022-01-15 12:19:15 -0500

    Before rebase

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const User = require('../models/users')
 
+//Creación de usuario
 const userRegister = async (req, res) => {
   const user = await User.create({...req.body})
   res.status(200).json(req.body)
