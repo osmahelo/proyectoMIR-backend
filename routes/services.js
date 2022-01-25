bfef2afee6af387ee8fbd01fca01b03ac9cf086d
Andres Puello <apuello1025@gmail.com>
2022-01-25 18:25:01 -0500

    Create Token & Customer in ePayco

--- routes/services.js
+++ routes/services.js
@@ -8,6 +8,7 @@ const {
   GetServices,
   GetServicesByCollab,
   SearchServices,
+  paymentService,
 } = require('../controllers/services');
 const router = express.Router();
 
@@ -15,5 +16,6 @@ router.route('/services').post(auth, CreateServices).get(GetServices);
 router.route('/service/:id').put(UpdateService).delete(DeleteService);
 router.route('/collaborator/:id/services').get(GetServicesByCollab);
 router.route('/search/services').get(SearchServices);
+router.route('/service/payment/user').post(paymentService);
 
 module.exports = router;
---
+++ utils/epayco.js
@@ -0,0 +1,8 @@
+const epayco = require('epayco-sdk-node')({
+  apiKey: process.env.PUBLIC_KEY,
+  privateKey: process.env.PRIVATE_KEY,
+  lang: 'ES',
+  test: true,
+});
+
+module.exports = epayco;
