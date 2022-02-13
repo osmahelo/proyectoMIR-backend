fde0dd380a8862f65c7c866f3f54aed50b13f95e
Andres Puello <apuello1025@gmail.com>
2022-02-13 01:19:47 -0500

    Error Handling & Delete Service

--- utils/emailCompare.js
+++ utils/emailCompare.js
@@ -1,13 +1,16 @@
 const User = require('../models/users');
 const Collaborator = require('../models/collaborator');
 
-const emailCompare = async (email) => {
+const emailExist = async (email) => {
   const emailCollab = await Collaborator.findOne({ email });
+  console.log(emailCollab);
   const emailUser = await User.findOne({ email });
+  console.log(emailUser);
   if (emailCollab || emailUser) {
+    console.log('entro aqui aunque no debia');
     return true;
   }
-  
+  return false;
 };
 
-module.exports = { emailCompare };
+module.exports = { emailExist };
